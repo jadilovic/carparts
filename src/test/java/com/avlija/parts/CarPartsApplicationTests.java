6045ae7b9fbbaf6386fe486a89878fd79e001e4c
jadilovic <adilovic79@yahoo.com>
2020-05-14 22:33:31 +0200

    first commit

---
+++ src/test/java/com/avlija/parts/CarPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avlija.parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
