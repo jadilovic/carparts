ece663b685cc889c7a807ea0ef882cbd6cbd36f0
jadilovic <adilovic79@yahoo.com>
2020-06-05 13:59:18 +0200

    final draft

--- src/main/java/com/avlija/parts/controller/SearchController.java
+++ src/main/java/com/avlija/parts/controller/SearchController.java
@@ -199,6 +199,7 @@ public class SearchController {
   CarModel selectedCarModel = carModelRepository.findByName(carModel.getName());
   System.out.println("SELECTED CAR Model " + selectedCarModel + carModel.getName());
   List<ProductGroup> productGroups = productGroupRepository.findAll();
+  productGroups.remove(0);
   SampleInputs inputs = new SampleInputs();
   inputs.setBrandName(selectedCarModel.getBrand().getName());
   inputs.setModelName(selectedCarModel.getName());
@@ -228,59 +229,5 @@ public class SearchController {
   model.setViewName("home/list_products");
   return model;
  }
- /*
- @RequestMapping(value= {"admin/signup"}, method=RequestMethod.GET)
- public ModelAndView signup() {
-  ModelAndView model = new ModelAndView();
-  User user = new User();
-  model.addObject("user", user);
-  model.setViewName("admin/signup");
-  
-  return model;
- }
  
- @RequestMapping(value= {"admin/signup"}, method=RequestMethod.POST)
- public ModelAndView createUser(@Valid User user, BindingResult bindingResult) {
-  ModelAndView model = new ModelAndView();
-  User userExists = userService.findUserByEmail(user.getEmail());
-  
-  if(userExists != null) {
-   bindingResult.rejectValue("email", "error.user", "This email already exists!");
-  }
-  if(bindingResult.hasErrors()) {
-   model.setViewName("user/signup");
-  } else {
-   userService.saveUser(user);
-   model.addObject("msg", "User has been registered successfully!");
-   model.addObject("user", new User());
-   model.setViewName("admin/signup");
-  }
-  return model;
- }
- 
- @RequestMapping(value= {"/admin/admin"}, method=RequestMethod.GET)
- public ModelAndView adminPage() {
-  ModelAndView model = new ModelAndView();
-  model.setViewName("admin/adminPage");
-  return model;
- }
- 
- @RequestMapping(value= {"/home/home"}, method=RequestMethod.GET)
- public ModelAndView home() {
-  ModelAndView model = new ModelAndView();
-  Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-  User user = userService.findUserByEmail(auth.getName());
-  
-  model.addObject("userName", user.getFirstname() + " " + user.getLastname());
-  model.setViewName("home/home");
-  return model;
- }
- 
- @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
- public ModelAndView accessDenied() {
-  ModelAndView model = new ModelAndView();
-  model.setViewName("errors/access_denied");
-  return model;
- }
- */
 }
