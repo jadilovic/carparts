314cae7145a77785ce9ab852793a40b8550e62f8
jadilovic <adilovic79@yahoo.com>
2020-12-05 00:03:09 +0100

    json woriking hard

--- src/main/resources/static/js/groups.js
+++ src/main/resources/static/js/groups.js
@@ -1,5 +1,6 @@
-
-/*
+/**
+ * http://usejsdoc.org/
+ */
 const menu = [
   {
     id: 0,
@@ -450,117 +451,25 @@ const menu = [
       	],
     }, 
 ];
-*/
 
-let menu;
-let tempMenu;
+// var myJSON = JSON.stringify(menu);
 
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-readTextFile("/file/groups.json", function(text){
-	menu = JSON.parse(text);
-	tempMenu = menu;
-	displayMenuButtons(menu);
-});
-
-window.addEventListener("DOMContentLoaded", function(){
-	 console.log(menu);
-});
-
-let prevMenu = [];
-
-const btnsContainer = document.querySelector(".cardy");
-
-//button display
-function displayMenuButtons(menuItems){
-	
-	const ids = [];
-	const categories = menuItems.reduce(function(values, item){
-		if(item.id > 0){
-			ids.push(item.id);
-		}
-			console.log(item.title);
-			values.push(item.title);
-			return values;
-		}, 
-		["Početno"],
-	);
-	
-	categories.push("Prethodno");
-	console.log(ids);
-	let categoryBtns;
-	
-	if(ids.length === 0){
-		categoryBtns = categories.map(function(category){
-			if(category === "Početno" || category === "Prethodno"){
-				return `<div class="card-body">
-				<button type="button" class="btn btn-success btn-block filter-btn" data-id='${category}'>${category}</button>
-				</div>`
-			} else {
-				return `<div class="card-body">
-				<button type="button" class="btn btn-primary btn-block filter-btn" data-id='${category}'>${category}</button>
-				</div>`
-			}
-		}).join("");
-		//console.log(categoryBtns);
-	} else {
-		let count = -2;
-		categoryBtns = categories.map(function(category){
-			count++;
-			if(count < 0 || count > ids.length - 1){
-				return `<div class="card-body">
-				<button type="button" class="btn btn-success btn-block filter-btn" data-id='${category}'>${category}</button>
-				</div>`
-			} else {
-				return `<div class="card-body">
-    			<button onclick="window.location='/home/listproducts/ + ${ids[count]}';" class="btn btn-primary btn-block">${category}</button>
-    			</div>`
-			}
-		}).join("");
-		//console.log(categoryBtns);
-	}
-
-	
-	btnsContainer.innerHTML = categoryBtns;
-	
-	const filterBtns = btnsContainer.querySelectorAll(".filter-btn");
-
-
-	// buttons
-	filterBtns.forEach(function(btn){
-		btn.addEventListener("click", function(e){
-
-		const titleName = e.currentTarget.dataset.id;
-		
-			// console.log(titleName);
-			let menuCategories = [];
-			tempMenu.forEach(function(menuObject){
-				if(menuObject.title === titleName){
-					menuCategories = menuObject.categories;
-					prevMenu = tempMenu;
-					tempMenu = menuObject.categories;
-				}
-			});
-
-			if(titleName === "Početno"){
-				displayMenuButtons(menu);
-				tempMenu = menu;
-			} else if(titleName === "Prethodno"){
-				displayMenuButtons(prevMenu);
-				tempMenu = prevMenu;
-			} else {
-				displayMenuButtons(menuCategories);
-			}
-		});
-	});
-}
+var saveData = (function () { 
+    var a = document.createElement("a"); 
+    document.body.appendChild(a); 
+    a.style = "display: none"; 
+    return function (data, fileName) { 
+        var json = JSON.stringify(data), 
+            blob = new Blob([json], {type: "octet/stream"}), 
+            url = window.URL.createObjectURL(blob); 
+        a.href = url; 
+        a.download = fileName; 
+        a.click(); 
+        window.URL.revokeObjectURL(url); 
+    }; 
+}()); 
+ 
+// var data = { x: 42, s: "hello, world", d: new Date() }, 
+let fileName = "groups.json"; 
+ 
+saveData(menu, fileName); 
